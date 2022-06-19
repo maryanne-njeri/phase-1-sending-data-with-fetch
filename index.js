28ad71088a94c8e995be04e994409e2be3977a1f
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-19 07:46:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+let name = "Steve";
+let email = "steve@steve.com";
+
+function submitData(name, email) {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", 
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(resp) {
+        return resp.json()
+    })
+    .then(function(data) {
+        return document.body.innerHTML = data.id;
+    })
+    .catch(function(error) {
+        return document.body.innerHTML = error.message;
+    })
+
+}
+
